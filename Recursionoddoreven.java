99e183ca3581a38b33ed6ef34896361f502c808b
Thrisha06 <thrishamadineni@gmail.com>
2026-04-09 11:50:48 +0530

    changedfile

---
+++ Recursionoddoreven.java
@@ -0,0 +1,14 @@
+public class Recursionoddoreven {
+    public static void main(String[] args) {
+        int arr[]={2,4,1,9,5,6,7};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                System.out.println(even);
+            }
+         }
+        }
+
+    }
+
+    
+
---
+++ perfectnumber.java
@@ -0,0 +1,19 @@
+public class perfectnumber {
+    public static void main(String[] args) {
+        for(int num=1;num<10000;num++){
+                int sum=0;
+        
+            for(int i=1;i<num;i++){
+            if(num%i==0){
+                sum += i;
+            } 
+        }
+           if(num==sum){
+                System.out.println(num+" ");
+            }
+        
+        }
+        
+    }
+    
+}
