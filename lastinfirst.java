bba5b9025c78a0a3ebba91450361fcc59876a1e8
Thrisha06 <thrishamadineni@gmail.com>
2026-04-10 11:14:10 +0530

    changedfiles

---
+++ lastinfirst.java
@@ -0,0 +1,8 @@
+public class lastinfirst {
+    public static void main(String[] args) {
+         String str = "Java";
+         String last  = str.substring(str.length()-1);
+         System.out.println(last + str + last);
+    }
+    
+}
